6617bbca4484bc5dd06587977a1e49d0abb8d5fc
William <williamcolin954@gmail.com>
2019-05-22 16:02:30 -0500

    Dias que se tenian antes de solicitar vacaciones

--- issues_leaves/__manifest__.py
+++ issues_leaves/__manifest__.py
@@ -14,7 +14,7 @@
     'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base,hr,hr_holidays'],
+    'depends': ['base','hr','hr_holidays'],
 
     # always loaded
     'data': [
--- issues_leaves/models/models.py
+++ issues_leaves/models/models.py
@@ -3,11 +3,11 @@
 from odoo import models, fields, api
 
 class issues_leaves(models.Model):
-    _name = 'hr.holidays'
+    _inherit = 'hr.leave'
 
-    days_before_approval = fields.Integer(related="holiday_status_id.remaining_leaves", string="Saldo de días antes de la aprobación")
+    days_before_approval = fields.Integer(string="Saldo de días antes de la aprobación")
 
-
-
-
-
+    @api.onchange('holiday_status_id')
+    def _onchange_days_before_approval(self):
+        if self.holiday_status_id:
+            self.days_before_approval = self.holiday_status_id.virtual_remaining_leaves
